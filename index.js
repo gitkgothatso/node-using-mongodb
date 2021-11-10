381a879a17f92fa9fd9938c3fd7bf7519d5398c1
gitkgothatso <k.seboya@gmail.com>
2021-11-10 07:21:04 +0200

    added db connection

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const PORT = 4000;
 
-// bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+function connect(){
+    const uri =
+      "mongodb+srv://adminuser:mniks@MDB90@cluster0.1hs7w.mongodb.net/productsdb?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
+
+    client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    });
+
+}
+
+app.use(express.json()); 
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
