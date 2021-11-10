49663bd1ef047041f08366ef91b42fd8030b54dd
gitkgothatso <k.seboya@gmail.com>
2021-11-10 07:29:45 +0200

    added db connection

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ const { MongoClient } = require("mongodb");
 const app = express();
 const PORT = 4000;
 
-function connect(){
-    const uri =
-      "mongodb+srv://adminuser:mniks@MDB90@cluster0.1hs7w.mongodb.net/productsdb?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    });
+const uri =
+    "mongodb+srv://adminuser:mniks@MDB90@cluster0.1hs7w.mongodb.net/productsdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
 
-    client.connect((err) => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-    });
-
-}
+client.connect((err) => {
+//const collection = client.db("test").collection("devices");
+// perform actions on the collection object
+//client.close();
+});
 
 app.use(express.json()); 
 
