d545739939aedc2eb7b7c43ad1742c530651678c
Kgothatso Seboya <k.seboya@kgstudio.net>
2021-11-11 20:35:44 +0200

    added routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,12 @@
-import {addnewProduct} from "../controllers/controllers";
+import {addnewProduct,getProducts,getProductWithID} from "../controllers/controllers";
 
 const routes = (app) =>{
-    app.route('/products').post(addnewProduct);
+    app.route('/products')
+    .post(addnewProduct)
+    .get(getProducts);
+
+    app.route('/products/:ProductID')
+    .get(getProductWithID);
 }
 
 export default routes;
